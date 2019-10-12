30c8e6f3878ce3027d6b4e14d02e94765fd010bb
Jesse Slavens <jesse.slavens@gmail.com>
2019-10-11 21:00:44 -0400

    Updates

---
+++ precache-manifest.3ad5f081420440beed91f4e3ab657ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b92c0e7fcd5cf51eb77b94366f65f5b",
+    "url": "/star-wars-people/index.html"
+  },
+  {
+    "revision": "acf90d0e99866a489376",
+    "url": "/star-wars-people/static/css/main.9bfb6d54.chunk.css"
+  },
+  {
+    "revision": "720595b523a059230396",
+    "url": "/star-wars-people/static/js/2.1e96c6d4.chunk.js"
+  },
+  {
+    "revision": "acf90d0e99866a489376",
+    "url": "/star-wars-people/static/js/main.65844354.chunk.js"
+  },
+  {
+    "revision": "321f6dc25bbac2b4e3eb",
+    "url": "/star-wars-people/static/js/runtime-main.e66a179e.js"
+  }
+]);
